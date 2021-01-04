373ba6a072755dd672114723846c1538ab0cc8fe
Aniket Singh <aniket1x11@gmail.com>
2021-01-05 03:06:51 +0530

    UPDATED added .gitignore and removed bin/ from staging

--- src/Driver.java
+++ src/Driver.java
@@ -5,5 +5,6 @@ public class Driver
 	{
 		System.out.println("Hello there. Initial code!");
 		System.out.println("Hello again! second commit code :) Sweet");
+		System.out.println("Looks like this will be becoming a habit :)");
 	}
 }
